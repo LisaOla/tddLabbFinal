8757afc13d233fc4838fe7e3c77ec4718f2a8f4f
Lisha <rashaalsawah@gmail.com>
2022-02-16 11:57:37 +0100

    Initial commit

---
+++ src/test/java/STaffSystemTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+
+public class STaffSystemTest {
+
+    @Test
+    void personalSystemetTest() {
+
+        StaffSystem staff = new StaffSystem();
+
+    }
+
+}
