8d8733860c01d8403a8a81dda6473603ef042118
Lisha <rashaalsawah@gmail.com>
2022-02-16 12:41:10 +0100

    GREEN

--- src/main/java/StaffSystem.java
+++ src/main/java/StaffSystem.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 
 public class StaffSystem {
+    public ArrayList<Employee> employeesArrayList = new ArrayList<Employee>();
     int autoIncrementId =0;
 
     public void addEmployee(Employee employee) {
         autoIncrementId++;
         employee.setCompanyId(autoIncrementId);
+        employeesArrayList.add(employee);
+
+
+
     }
 }
