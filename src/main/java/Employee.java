12fd82a63d7c7318428a7cf73e0d7fd3650ebba3
Lisha <rashaalsawah@gmail.com>
2022-02-16 12:11:41 +0100

    REFACTOR

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,21 +1,40 @@
 public class Employee {
+    String name;
+    String lastName;
+    int age;
 
-    public Employee(String nils, String ericsson, int i) {
-    }
 
     public Employee() {
 
     }
 
+    public Employee(String name, String lastName, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
     public String getName() {
-        return "Nils";
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getLastName() {
-        return "Ericsson";
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getAge() {
-        return 47;
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
